62d6ee2ac6c95c654a22a32191b2ecedd614151a
Maurizio <proiettomaurizio97@gmail.com>
2022-02-14 14:33:33 +0100

    create scafolling home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -16,17 +10,12 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const app = new Vue({
+        el: '#app',
+    });    
+  });
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main-layouts')
+@section('content')
+    hello world
+    <example-component></example-component>
+@endsection
