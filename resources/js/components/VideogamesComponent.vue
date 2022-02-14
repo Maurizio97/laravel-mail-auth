683bd737c3213eef78626d06b2a9b64ac4019d97
Maurizio <proiettomaurizio97@gmail.com>
2022-02-14 15:03:41 +0100

    create scaffolding component videogames

---
+++ resources/js/components/VideogamesComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <table border="1">
+            <tr>
+                <th>Title</th>
+                <th>Subtitle</th>
+                <th>Rating</th>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    mounted() {
+        axios.get()
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    table {
+        margin: 0 auto;
+
+        th {
+            padding: 5px 15px;
+        }
+
+        td {
+            padding: 5px 15px;
+        }
+    }
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', 'GuestController@home') -> name('home');
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
+Route::post('/register', 'Auth\RegisterController@register') -> name('register');
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
 
-// Auth::routes();
-
-// Route::get('/', 'HomeController@index')->name('home');
