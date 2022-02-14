e71be470cdf6810aa6e3cf7c211f5fd8928ff5fe
Maurizio <proiettomaurizio97@gmail.com>
2022-02-14 14:13:41 +0100

    create and complete migration

--- database/migrations/2022_02_14_130058_create_videogames_table.php
+++ database/migrations/2022_02_14_130058_create_videogames_table.php
@@ -14,8 +14,13 @@ class CreateVideogamesTable extends Migration
     public function up()
     {
         Schema::create('videogames', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table -> id();
+
+            $table -> string('title', 60);
+            $table -> string('subtitle',40);
+            $table -> integer('rating');
+
+            $table -> timestamps();
         });
     }
 
